fa65ab1658c2245c2efee7443f5313327c057b64
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-09-25 12:52:02 -0400

    Fixing typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# inherit from common milletl
+# inherit from common millet
 include device/samsung/millet-common/BoardConfigCommon.mk
 
 # Assert
